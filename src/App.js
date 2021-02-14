0a544a1fdefeebe8ba73cbd280fdfa75c62fd478
jernber <oraclemars@gmail.com>
2021-02-13 18:17:35 -0600

    setup chatengine

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 import { ChatEngine } from 'react-chat-engine'
 
 import './App.css'
+
+const App = () => {
+    return(
+     <ChatEngine 
+        height="100vh"
+        projectID="bb5103f6-de28-4c32-99af-3c05acc2f53d"
+        userName="jbo"
+        userSecret="123123"
+     />   
+    )
+}
+
+export default App
