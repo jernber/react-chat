711c2c7a19a386542cc0d816749d3d0ac6f981a4
jernber <oraclemars@gmail.com>
2021-02-13 20:17:55 -0600

    handled auth of user

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+const LoginForm = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const authObject = { 'Project-ID': 'bb5103f6-de28-4c32-99af-3c05acc2f53d', 'User-Name': username, 'User-Secret': password }
+        try {
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject })
+
+            localStorage.setItem('username', username)
+            localStorage.setItem('password', password)
+            
+            window.location.reload()
+        } catch (error) {
+            
+        }
+        
+    }
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">John Bo Chatroom</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required/>
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm
